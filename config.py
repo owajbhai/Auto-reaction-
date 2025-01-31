5b3dd6761bbd5ec03a2e65ecd282206ac918f009
kane5 <Nguyenkhactam5@gmail.com>
2025-02-01 05:43:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "")
-API_ID = int(environ.get("API_ID", ""))
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_OWNER = int(environ.get("BOT_OWNER", ""))
-BOT_USERNAME = environ.get("BOT_USERNAME", "QuickReactRobot")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
-DATABASE_URL = environ.get("DATABASE_URL", "")
+API_HASH = environ.get("API_HASH", "72cc7062951f56abb5240994690e49de")
+API_ID = int(environ.get("API_ID", "25811007"))
+BOT_TOKEN = environ.get("BOT_TOKEN", "7789014828:AAHCFG1-ZswdY8nJUAC4HAwnUrT3O0GWxjE")
+BOT_OWNER = int(environ.get("BOT_OWNER", "7240555847"))
+BOT_USERNAME = environ.get("BOT_USERNAME", "tymchomapbot")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002236728519"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002271972643"))
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://nguyenkhactam5:johnmahkcohbk5@kanereaction.zhqqd.mongodb.net/?retryWrites=true&w=majority&appName=kanereaction")
 
 # Define default emojis list
 EMOJIS = [
