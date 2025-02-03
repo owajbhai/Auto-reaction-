25778e490ff36a1d864757a124953cceed671138
kane5 <Nguyenkhactam5@gmail.com>
2025-02-04 06:21:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_HASH = environ.get("API_HASH", "72cc7062951f56abb5240994690e49de")
 API_ID = int(environ.get("API_ID", "25811007"))
-BOT_TOKEN = environ.get("BOT_TOKEN", "7789014828:AAHCFG1-ZswdY8nJUAC4HAwnUrT3O0GWxjE")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7789014828:AAF_85sQOhwProRoVKGuqidjFAlHCQZtUCo")
 BOT_OWNER = int(environ.get("BOT_OWNER", "7240555847"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "tymchomapbot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002465408399"))
