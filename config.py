1e28c51488056076f7370b1631051ff71dfd0d47
kane5 <Nguyenkhactam5@gmail.com>
2025-02-03 15:10:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(environ.get("API_ID", "25811007"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "7789014828:AAHCFG1-ZswdY8nJUAC4HAwnUrT3O0GWxjE")
 BOT_OWNER = int(environ.get("BOT_OWNER", "7240555847"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "tymchomapbot")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002236728519"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002465408399"))
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002271972643"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://nguyenkhactam5:q1231234@cluster0.srj0a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
