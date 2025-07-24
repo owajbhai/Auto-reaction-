483fa0c0f7a4b116242b6013c70d7285c67b4a8a
owajbhai <owajshaikhrcm@gmail.com>
2025-07-25 00:29:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ PICS = (os.environ.get("PICS", "")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001860172104"))
 
-DB_URI = os.environ.get("DB_URI", "Client")
+DB_URI = os.environ.get("DB_URI", "cluster0")
 DB_NAME = os.environ.get("DB_NAME", "mongodb+srv://sisatmpnjnyx:pTlX7jKKpHlh0c0z@cluster0.sg46eod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
