130241d48d621dd904b17f60054e0a0d006c2e3c
owajbhai <owajshaikhrcm@gmail.com>
2025-07-24 13:47:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ PICS = (os.environ.get("PICS", "")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001860172104"))
 
-DB_URI = os.environ.get("DB_URI", "Cluster0")
+DB_URI = os.environ.get("DB_URI", "Cluster0737374838")
 DB_NAME = os.environ.get("DB_NAME", "mongodb+srv://Botmaster2ndmongo:Botmaster2ndmongo@cluster0737374838.gbhwiw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0737374838")
 
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
