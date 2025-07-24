c7da5d5ff5cf40ec1b2af23f0c1b502fcb1b120e
owajbhai <owajshaikhrcm@gmail.com>
2025-07-24 13:52:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ PICS = (os.environ.get("PICS", "")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001860172104"))
 
-DB_URI = os.environ.get("DB_URI", "Cluster0737374838")
-DB_NAME = os.environ.get("DB_NAME", "mongodb+srv://Botmaster2ndmongo:Botmaster2ndmongo@cluster0737374838.gbhwiw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0737374838")
+DB_URI = os.environ.get("DB_URI", "Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "mongodb+srv://Professor:ProfessorSourabh@cluster0.88hrsye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002046895970").split())) # Add Multiple channel ids
